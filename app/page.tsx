7f7353127095b36ab8e5bf8bc05ba46998d67217
Hayyuu <hayatahimed518@gmail.com>
2025-07-11 01:58:26 +0300

    update fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function Page() {
